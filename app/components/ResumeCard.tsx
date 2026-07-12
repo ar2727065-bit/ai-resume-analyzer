2b50a44c18ee1d305e340a921d8821333347879a
Abhay Verma <ar2727065@gmail.com>
2026-07-12 16:10:02 +0530

    Authentication

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router'
+import ScoreCircle from './ScoreCircle'
+
+function ResumeCard({ resume }: { resume: Resume}) {
+  return (
+     <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000">
+            <div className="resume-card-header">
+                <div className="flex flex-col gap-2">
+                    {resume.companyName && <h2 className="!text-black font-bold break-words">{resume.companyName}</h2>}
+                    {resume.jobTitle && <h3 className="text-lg break-words text-gray-500">{resume.jobTitle}</h3>}
+                    {!resume.companyName && !resume.jobTitle && <h2 className="!text-black font-bold">Resume</h2>}
+                </div>
+                <div className="flex-shrink-0">
+                    <ScoreCircle score={resume.feedback.overallScore} />
+                </div>
+            </div>
+            {resume.resumePath && (
+                <div className="gradient-border animate-in fade-in duration-1000">
+                    <div className="w-full h-full">
+                        <img
+                            src={resume.imagePath || "/public/images/resume-placeholder.png"}
+                            alt="resume"
+                            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                        />
+                    </div>
+                </div>
+                )}
+        </Link>
+  )
+}
+
+export default ResumeCard
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import path from "path/win32";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    route("/auth", "routes/auth.tsx"),
+] satisfies RouteConfig;
+        
